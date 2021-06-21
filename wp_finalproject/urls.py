6e3466d8f0f13f9ef1e546b4451ff4773a701660
ruociyu <m6bji4h6@gmail.com>
2021-06-21 23:32:53 +0800

    django-react_2

--- wp_finalproject/urls.py
+++ wp_finalproject/urls.py
@@ -18,8 +18,8 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html')),
+    path(r'', admin.site.urls),
+    path(r'', TemplateView.as_view(template_name='index.html')),
 ]
 
 # urlpatterns += [
