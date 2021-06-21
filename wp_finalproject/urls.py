a7a1a91e8bff5c94ee36bf5416032f4d2965399b
ruociyu <m6bji4h6@gmail.com>
2021-06-22 03:01:41 +0800

    django

--- wp_finalproject/urls.py
+++ wp_finalproject/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path(r'', admin.site.urls),
+    path('admin/', admin.site.urls),
     path(r'', TemplateView.as_view(template_name='index.html')),
 ]
 
